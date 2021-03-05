12b8f7daa2126c775b7410d5003ee96c8f5248a6
Deidrah <dominikastasik93@gmail.com>
2021-03-05 09:15:25 +0100

    adding simple paper rock scissors logic

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if ( computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Przegrałeś :(');
+} else if ( computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('Ty wygrywasz!'); 
+} else if ( computerMove == 'kamień' && playerMove == 'nożyce') {
+  printMessage('Przegrałeś :('); 
+} else if ( computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('Przegrałeś :('); 
+} else if ( computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('Ty wygrywasz!'); 
+} else if ( computerMove == 'papier' && playerMove == 'papier') {
+  printMessage('Remis'); 
+} else if ( computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage('Remis'); 
+} else if ( computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('Remis'); 
+} else {
+  printMessage('nieznany ruch');
+}
